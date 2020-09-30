9691d90ecd23b397f7f037736fec257799beca3c
lehoux mathieu <mathieu.lehoux@orange.com>
2020-10-01 00:10:31 +0200

    adding test for change mode (eco mode enabled)

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -77,3 +77,27 @@ async def test_set_target_temperature(event_loop, aresponses):
             mac="AABBCCDDEEFF", password="1234", session=session, loop=event_loop,
         )
         await fumis.set_target_temperature(23.4)
+
+@pytest.mark.asyncio
+async def test_set_mode(event_loop, aresponses):
+    """Test setting mode of a Fumis WiRCU device."""
+    # Handle to run asserts on request in
+    async def response_handler(request):
+        data = await request.json()
+        assert data == {
+            "unit": {"id": "AABBCCDDEEFF", "type": 0, "pin": "1234"},
+            "apiVersion": "1",
+            "controller": {"ecoMode": {"ecoModeEnable": 1}},
+        }
+
+        return aresponses.Response(
+            status=200, headers={"Content-Type": "application/json"}, text="",
+        )
+
+    aresponses.add("api.fumis.si", "/v1/status/", "POST", response_handler)
+
+    async with aiohttp.ClientSession(loop=event_loop) as session:
+        fumis = Fumis(
+            mac="AABBCCDDEEFF", password="1234", session=session, loop=event_loop,
+        )
+        await fumis.set_mode(1)
